8a3613f036724e387049388e87ea13ca67fc1291
Rafaella <fariassrafaella@gmail.com>
2025-03-16 16:06:21 -0300

    initial commit

---
+++ src/test/java/barbershop/barbershop_api/BarbershopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package barbershop.barbershop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
